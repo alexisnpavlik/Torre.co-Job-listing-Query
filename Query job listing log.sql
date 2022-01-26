a40cde3f6e4da1c858c14185076b82720f8f0942
Alexis Medina <alexisnpavlik@gmail.com>
2022-01-25 23:32:55 -0300

    Update Query job listing log.sql

--- Query job listing log.sql
+++ Query job listing log.sql
@@ -1,24 +1,26 @@
 SELECT
     o.id as ID,
-    o.objective as Job_title,
+    o.objective as 'Job title',
     -- url,
     -- Manager_link,
     -- location,
-    o.created as Created_date,
-    o.reviewed as Approved_date,
+    o.created as 'Created date',
+    o.reviewed as 'Approved date',
     -- closing date,
-    o.locale as Language_of_the_post,
-    o.commitment_id as Type_of_job,
-    o.fulfillment as Type_of_service,
+    o.locale as 'Language of the post',
+    o.commitment_id as 'Type of job',
+    o.fulfillment as 'Type of service',
     -- Recruiter_advisor,
-    o.status as Status,
-    o.last_updated as Changes_history,
-    o. as Completed_applications
+    o.status as 'Status',
+    o.last_updated as 'Changes history',
+    sum(case when oc.id is not null and oc.interested is not null then 1 else 0 end) as 'Completed Applications'
     
     
     
 
 FROM opportunities as o
+
+left join opportunity_candidates as oc on o.id = oc.opportunity_id
 
 WHERE true
     and objective <> 'Shared by an intermediary'
