29d0d86174203e10570a889063de5441cb772d64
Alexis Medina <alexisnpavlik@gmail.com>
2022-01-26 17:47:31 -0300

    Update Query part_2.sql

--- Query part_2.sql
+++ Query part_2.sql
@@ -16,6 +16,8 @@ inner join member_evaluations me on oc.interested = me.interested
 WHERE true
     and o.objective <> 'Shared by an intermediary'
     and o.review = 'approved'
+    and o.reviewed is not null
+    
 group by o.id
 order by o.created desc
 LIMIT 10000;
