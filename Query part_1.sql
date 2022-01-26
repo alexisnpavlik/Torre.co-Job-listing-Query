8039eb537b2607b981eee4e08a64723a9a5d9d44
Alexis Medina <alexisnpavlik@gmail.com>
2022-01-26 17:22:13 -0300

    Update Query part_1.sql

--- Query part_1.sql
+++ Query part_1.sql
@@ -19,6 +19,7 @@ join opportunity_changes_history as och on o.id = och.opportunity_id
 WHERE true
     and o.objective <> 'Shared by an intermediary'
     and o.review = 'approved'
+    and o.reviewed is not null
 group by o.id
 order by o.created desc
 LIMIT 10000;
