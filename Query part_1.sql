2891fca4eec07530a2f00b82888913e232e5f4d0
Alexis Medina <alexisnpavlik@gmail.com>
2022-01-27 16:50:19 -0300

    Update Query part_1.sql

--- Query part_1.sql
+++ Query part_1.sql
@@ -5,7 +5,7 @@ SELECT
     o.objective as 'Job title',
     -- location
     (select group_concat(l.location) from opportunity_places l where l.opportunity_id = o.id and l.active = 1) as 'location',
-    --Created date 
+    -- Created date 
     o.created as 'Created date',
     -- Approved date
     o.reviewed as 'Approved date',
@@ -17,23 +17,31 @@ SELECT
     o.fulfillment as 'Type of service',
     -- Status
     o.status as 'Status',
-    -- Changes history
+    -- Account Manager
+    p.username as 'Account manager',
+    -- Changes history, last updated
     o.last_updated as 'Changes history',
-
-    pam.person_id as 'Account manager',
+    -- Completed applications
     sum(case when oc.id is not null and oc.interested is not null then 1 else 0 end) as 'Completed Applications',
+    -- Incomplete applications
     sum(case when oc.id is not null and oc.interested is null and application_step is not null then 1 else 0 end) as 'Incomplete applications'
+
 FROM opportunities as o
 
+-- Necessary for completed applications and incomplete applications
 join opportunity_candidates as oc on o.id = oc.opportunity_id
+-- Necessary for account manager
 join opportunity_changes_history as och on o.id = och.opportunity_id
 left join person_account_managers as pam on och.person_id = pam.person_id
+left join people as p on pam.person_id = p.id
 
 
 WHERE true
     and o.objective <> 'Shared by an intermediary'
     and o.review = 'approved'
+    -- Approved date is not null
     and o.reviewed is not null
+
 group by o.id
 order by o.created desc
 LIMIT 10000;
