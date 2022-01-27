2fe047ccde7f122452e07ecd1de5bd14a6e7e5f7
Alexis Medina <alexisnpavlik@gmail.com>
2022-01-27 18:53:35 -0300

    Update Query job listing log.sql

--- Query job listing log.sql
+++ Query job listing log.sql
@@ -1,51 +1,87 @@
 SELECT
-    
+    -- ID
     o.id as ID,
+    -- Job title
     o.objective as 'Job title',
-    -- url,
-    -- Manager_link,
+    -- location
     (select group_concat(l.location) from opportunity_places l where l.opportunity_id = o.id and l.active = 1) as 'location',
-    o.created as 'Created date',
-    o.reviewed as 'Approved date',
-    o.deadline as 'Closing date',
-    o.locale as 'Language of the post',
-    o.commitment_id as 'Type of job',
-    o.fulfillment as 'Type of service',
+    -- Commit date
     och.created as 'Commited date',
-    me.send_disqualified_notification = 'True' as 'Disqualified',
-    pam.person_id as 'Account manager',
-    (select o.reviewed where not null) as 'apobated date'
-    -- Recruiter_advisor,
+    -- Created date 
+    o.created as 'Created date',
+    -- Approved date
+    o.reviewed as 'Approved date',
+    -- Language of the post
+    o.locale as 'Language of the post',
+    -- Type of job
+    o.commitment_id as 'Type of job',
+    -- Type of service
+    o.fulfillment as 'Type of service',
+    -- Status
     o.status as 'Status',
+    -- Account Manager
+    p.username as 'Account manager',
+    -- filter 
+     me.send_disqualified_notification  as 'data',
+    -- Changes history, last updated
     o.last_updated as 'Changes history',
+    -- Completed applications
     sum(case when oc.id is not null and oc.interested is not null then 1 else 0 end) as 'Completed Applications',
-    sum(case when oc.id is not null and oc.interested is null and application_step is not null then 1 else 0 end) as 'Incomplete applications',
-    sum(case when oc.id is not null and osh.hiring_date is not null then 1 else 0 end) as 'Hires'
-    -- Pending for review
-    -- Mutual matches
-    -- Other
-    -- Active
-    -- Disqualified
+    -- Incomplete applications
+    sum(case when oc.id is not null and oc.interested is null and application_step is not null then 1 else 0 end) as 'Incomplete applications'
     -- Hires
-    -- Yesterday new applications
-    -- Superfilter
-    -- +2 weeks with Superfilter
-    -- +2 weeks with -10 appl
+    sum(case when oc.id is not null and osh.hiring_date is not null then 1 else 0 end) as 'Hires'
+
+FROM opportunities as o
+
+-- Necessary for completed applications and incomplete applications
+join opportunity_candidates as oc on o.id = oc.opportunity_id
+-- Necessary for account manager
+join opportunity_changes_history as och on o.id = och.opportunity_id
+--Necessa
+left join person_account_managers as pam on och.person_id = pam.person_id
+--join to make the relation people-person_account_mannager
+left join people as p on pam.person_id = p.id
+--join to asign osh to get match dates
+left join opportunity_stats_hires osh on o.id = osh.opportunity_id
+--join to get member evaluation
+join member_evaluations as me on oc.id = me.member_id
+
+
+WHERE true
+    and me.send_disqualified_notification = true
+    and o.objective <> 'Shared by an intermediary'
+    and o.review = 'approved'
+    -- Approved date is not null
+    and o.reviewed is not null
+    and p.username in ("nataliaarrieta","danna73","josealdave","davidorban","srlondono","najarroa","andresmeza","danielaavila","natisropi","danielabotero","carolinamontoyaar","valentinasantosss","danigaviriaz24","jhonmarin1989","nellcacho","jhonmarin","diana_cujer","dianaaparicio ","edlombanal","danna73","paulamarulanda","pklozanom","melissa")
+
+group by o.id
+order by o.created desc;
+
+
+
+
+
+
+SELECT
+    o.id as ID_3,
+    me.send_disqualified_notification  as 'data'#
     
-    
+   
 
 FROM opportunities as o
 
 join opportunity_candidates as oc on o.id = oc.opportunity_id
-join opportunity_changes_history as och on o.id = och.opportunity_id
-left join opportunity_stats_hires osh on o.id = osh.opportunity_id
-left join person_account_managers as pam on och.person_id = pam.person_id
-inner join member_evaluations me on oc.interested = me.interested
+join member_evaluations as me on oc.id = me.member_id
 
-WHERE true
+WHERE me.send_disqualified_notification = true#
     and o.objective <> 'Shared by an intermediary'
     and o.review = 'approved'
-
+    and o.reviewed is not null
+    
+    
+    
 group by o.id
 order by o.created desc
 LIMIT 10000;
