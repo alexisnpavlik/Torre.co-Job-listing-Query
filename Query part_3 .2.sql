e4fe43d1867be069a6ed281665e3e0c764dc4aac
MaoPerucho <70913789+MaoPerucho@users.noreply.github.com>
2022-01-27 16:27:02 -0500

    Update Query part_3 .2.sql
    it works

--- Query part_3 .2.sql
+++ Query part_3 .2.sql
@@ -2,8 +2,8 @@ SELECT
     o.id as ID_3,
     me.send_disqualified_notification  as 'data',
     me.interested as 'fecha1',
-    oc.interested as 'fecha2'
-    
+    oc.interested as 'fecha2',
+    (case when me.interested is not null and oc.interested is not null then 1 else 0 end) as 'Match'
    
 
 FROM opportunities as o
@@ -12,6 +12,7 @@ join opportunity_candidates as oc on o.id = oc.opportunity_id
 join member_evaluations as me on oc.id = me.member_id
 
 WHERE me.send_disqualified_notification = true
+   
     and o.objective <> 'Shared by an intermediary'
     and o.review = 'approved'
     and o.reviewed is not null
