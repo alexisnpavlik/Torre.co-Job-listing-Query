2f498f6621cfb111f4009aa28d3554be929bbec6
Alexis Medina <alexisnpavlik@gmail.com>
2022-01-27 15:14:11 -0300

    Update Query part_1.sql

--- Query part_1.sql
+++ Query part_1.sql
@@ -11,10 +11,15 @@ SELECT
     o.reviewed as 'Approved date',
     -- Language of the post
     o.locale as 'Language of the post',
+    -- Type of job
     o.commitment_id as 'Type of job',
+    -- Type of service
     o.fulfillment as 'Type of service',
+    -- Status
     o.status as 'Status',
+    -- Changes history
     o.last_updated as 'Changes history',
+
     pam.person_id as 'Account manager',
     sum(case when oc.id is not null and oc.interested is not null then 1 else 0 end) as 'Completed Applications',
     sum(case when oc.id is not null and oc.interested is null and application_step is not null then 1 else 0 end) as 'Incomplete applications'
