ac6fe7d5d21194e3e53de1c8d1273fc436851468
Alexis Medina <alexisnpavlik@gmail.com>
2022-02-01 16:49:53 -0300

    Update main_query.sql

--- main_query.sql
+++ main_query.sql
@@ -51,13 +51,11 @@ SELECT
     and oc2.name <> 'mutual matches'
     and (last_evaluation.last_interest is not null and (last_evaluation.last_not_interest is null or last_evaluation.last_interest > last_evaluation.last_not_interest)) then 1 else 0 end)  as 'Active',
     -- Pending for review
-    ( sum(case when oc.interested is not null then 1 else 0 end) - ( sum(case when me.interested is not null and oc.interested is not null then 1 else 0 end) + sum(case when oc.id is not null and oc.interested is not null and oc.column_id is not null
+     sum(case when oc.interested is not null then 1 else 0 end) - ( sum(case when me.interested is not null and oc.interested is not null then 1 else 0 end) + sum(case when oc.id is not null and oc.interested is not null and oc.column_id is not null
     and oc2.name <> 'mutual matches'
-    and (last_evaluation.last_interest is not null and (last_evaluation.last_not_interest is null or last_evaluation.last_interest > last_evaluation.last_not_interest)) then 1 else 0 end) + me.send_disqualified_notification ) ) as 'Pending for review',
+    and (last_evaluation.last_interest is not null and (last_evaluation.last_not_interest is null or last_evaluation.last_interest > last_evaluation.last_not_interest)) then 1 else 0 end) + me.send_disqualified_notification )  as 'Pending for review',
     -- Hires
     DATE(osh.hiring_date) as 'Hires',
-
-
     -- Sharing token
     (select sharing_token from opportunity_members where manager = true and status = 'accepted' and opportunity_id =  o.id  limit 1) as 'Sharing token'
 
@@ -83,7 +81,6 @@ FROM opportunities as o
     left join ( select me.candidate_id, max(me.interested) as last_interest, max(me.not_interested) as last_not_interest
     from member_evaluations me group by me.candidate_id) last_evaluation on last_evaluation.candidate_id = oc.id
       
-    
 
 WHERE true
     #and me.send_disqualified_notification = true
@@ -92,7 +89,6 @@ WHERE true
     -- Approved date is not null
     and o.reviewed is not null
     and osh.hiring_date is not null
-    and p.username in is not null
-
+     and p.username is not null
 group by o.id
 order by o.created desc;
