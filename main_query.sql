571eda6a981a0a37b2fc3eee89733d0187658bee
Alexis Medina <alexisnpavlik@gmail.com>
2022-02-02 16:47:18 -0300

    Update main_query.sql

--- main_query.sql
+++ main_query.sql
@@ -33,7 +33,7 @@ SELECT
     sum(case when oc.id is not null and oc.interested is not null and oc.column_id is not null
     and oc2.name <> 'mutual matches'
     and (last_evaluation.last_interest is not null and (last_evaluation.last_not_interest is null or last_evaluation.last_interest > last_evaluation.last_not_interest)) then 1 else 0 end)
-    as 'Other',
+    as 'Others',
     sum(case when oc.id is not null and oc.interested is not null
     and (last_evaluation.last_not_interest is not null and (last_evaluation.last_interest is null or last_evaluation.last_interest < last_evaluation.last_not_interest)) then 1 else 0 end)
     as 'Disqualified',
