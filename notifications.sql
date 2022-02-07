61e01b3785699578d97cbdb1c4da7243000fefae
Alexis Medina <alexisnpavlik@gmail.com>
2022-02-07 20:18:56 -0300

    Update notifications.sql

--- notifications.sql
+++ notifications.sql
@@ -4,4 +4,4 @@ select
     count(distinct model = 'signal_person_all') as 'Sent SigPeop',
     count(distinct model = 'signal_organization_all') as 'Sent SigOrg'
 from com_torrelabs_match_distributed_2
-group by opportunity_ref;
+group by opportunity_ref;sd
