092e3632b69cdca7b4c2186f3887d2b1e68903b6
Alexis Medina <alexisnpavlik@gmail.com>
2022-02-07 20:19:53 -0300

    Update notifications.sql

--- notifications.sql
+++ notifications.sql
@@ -1,7 +1,9 @@
 select
-    distinct opportunity_ref as 'Alfa ID',
-    count(distinct model = 'realistic') as 'Sent Matches',
-    count(distinct model = 'signal_person_all') as 'Sent SigPeop',
-    count(distinct model = 'signal_organization_all') as 'Sent SigOrg'
+    distinct opportunity_ref as AlfaID,
+    count(distinct model = 'realistic') as realistic,
+    count(distinct model = 'signal_person_all') as person,
+    count(distinct model = 'signal_organization_all') as organization
+    
 from com_torrelabs_match_distributed_2
-group by opportunity_ref;sd
+
+group by AlfaID;
