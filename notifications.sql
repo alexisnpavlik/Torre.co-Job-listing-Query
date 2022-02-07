04ab9bf345cf94c747222fb178d2dcbd675784b5
laumc98 <lanaca98@gmail.com>
2022-02-07 18:02:26 -0500

    Create notifications.sql

---
+++ notifications.sql
@@ -0,0 +1,7 @@
+select
+    distinct opportunity_ref as AlfaID,
+    count(distinct model = 'realistic') as realistic,
+    count(distinct model = 'signal_person_all') as person,
+    count(distinct model = 'signal_organization_all') as organization
+from com_torrelabs_match_distributed_2
+group by AlfaID
