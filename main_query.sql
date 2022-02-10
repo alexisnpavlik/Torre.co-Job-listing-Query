783461a18cc356ba5a2d8242dfc3c929993055b1
Alexis Medina <alexisnpavlik@gmail.com>
2022-02-09 21:39:02 -0300

    Update main_query.sql

--- main_query.sql
+++ main_query.sql
@@ -24,22 +24,14 @@ SELECT
     -- Completed applications
     sum(case when oc.id is not null and oc.interested is not null then 1 else 0 end) as 'Completed applications',
     -- Completed applications yesterday
-    sum(case when oc.id is not null and oc.interested is not null and DATE(oc.created) = DATE(NOW() - INTERVAL 1 DAY) then 1 else 0 end) as 'Completed applications yesterday',
+    sum(case when oc.id is not null and oc.interested is not null and DATE(oc.created) = DATE(DATE(NOW()) - INTERVAL 1 DAY) then 1 else 0 end) as 'Completed applications yesterday',
     -- Incomplete applications
     sum(case when oc.id is not null and oc.interested is null and application_step is not null then 1 else 0 end) as  'Incomplete applications',
-    -- Incomplete applications yesterday
-    sum(case when oc.id is not null and oc.interested is null and application_step is not null and DATE(oc.created) = DATE(NOW() - INTERVAL 1 DAY) then 1 else 0 end) as 'Incomplete applications yesterday',
     -- Mutual matches
     sum(case when oc.id is not null and oc.interested is not null and oc.column_id is not null
     and oc2.name = 'mutual matches'
     and (last_evaluation.last_interest is not null and (last_evaluation.last_not_interest is null or last_evaluation.last_interest > last_evaluation.last_not_interest)) then 1 else 0 end)
     as 'Mutual matches',
-    -- Mutual matches yesterday
-    sum(case when oc.id is not null and oc.interested is not null and oc.column_id is not null
-    and oc2.name = 'mutual matches'
-    and (last_evaluation.last_interest is not null and (last_evaluation.last_not_interest is null or last_evaluation.last_interest > last_evaluation.last_not_interest))
-    and DATE(oc.created) = DATE(NOW() - INTERVAL 1 DAY) then 1 else 0 end)
-    as 'Mutual matches yesterday',
     -- Others
     sum(case when oc.id is not null and oc.interested is not null and oc.column_id is not null
     and oc2.name <> 'mutual matches'
@@ -57,7 +49,9 @@ SELECT
     -- Hires
     (select sum(case when osh.hiring_date is not null then 1 else null end) from opportunity_stats_hires osh where o.id=osh.opportunity_id) as 'Hires',
     -- Hires yesterday
-    (select sum(case when osh.hiring_date is not null and DATE(osh.hiring_date) = DATE(DATE(NOW()) - INTERVAL 1 DAY) then 1 else null end) from opportunity_stats_hires osh where o.id=osh.opportunity_id) as 'Hires yesterday',
+    (select sum(case when osh.hiring_date is not null and DATE(osh.hiring_date) = DATE(DATE(NOW()) - INTERVAL 1 DAY) then true else null end) from opportunity_stats_hires osh where o.id=osh.opportunity_id) as 'Hires yesterday',
+    -- opportunity aprroved yesterday
+   sum(case when DATE(o.reviewed) = DATE(DATE(NOW()) - INTERVAL 1 DAY) then 1 else null end) as 'opportunity aprroved yesterday',
     -- Sharing token
     (select sharing_token from opportunity_members where manager = true and status = 'accepted' and opportunity_id =  o.id  limit 1) as 'Sharing token'
 
