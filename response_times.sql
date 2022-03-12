b9a61788312d9b84b35bae6c04451da0b5202be6
Alexis Medina <alexisnpavlik@gmail.com>
2022-03-12 13:54:57 -0300

    s

--- response_times.sql
+++ response_times.sql
@@ -3,12 +3,11 @@ select
    date(opportunities.reviewed) as approved_date,
    date(opportunity_candidates.interested) as interested_date,
    `Tracking Codes`.`utm_medium` as utm_medium,
-    (select timestampdiff(day, approved_date,min(interested_date)) where interested_date is not null and utm_medium = 'srh_jobs') as 'Response Time Job Search',
-    (select timestampdiff(day, approved_date,min(interested_date)) where interested_date is not null and utm_medium = 'ja_mtc') as 'Response Time Job Alerts from Preferences Matches',
-    (select timestampdiff(day, approved_date,min(interested_date)) where interested_date is not null and utm_medium = 'rc_trrx_inv') as 'Response Time Manual invite by Torrex of registered candidates (internal sourcing)',
-    (select timestampdiff(day, approved_date,min(interested_date)) where interested_date is not null and utm_medium = 'rc_cb_rcdt') as 'Response Time Career Advisor'
+   min(IF (date(opportunities.reviewed) < date(opportunity_candidates.interested), timestampdiff(day, date(opportunities.reviewed),date(opportunity_candidates.interested)), (timestampdiff(day, date(opportunity_candidates.interested),date(opportunities.reviewed))*-1))) AS 'RT'
 FROM opportunity_candidates
 LEFT JOIN `tracking_code_candidates` `Tracking Code Candidates - Person` ON `opportunity_candidates`.`id` = `Tracking Code Candidates - Person`.`candidate_id`
 LEFT JOIN `tracking_codes` `Tracking Codes` ON `Tracking Code Candidates - Person`.`tracking_code_id` = `Tracking Codes`.`id`
 LEFT JOIN opportunities on opportunities.id = opportunity_candidates.opportunity_id
-group by  ID
+WHERE opportunity_candidates.interested is not null AND utm_medium IN ('ja_mtc','ja_rlvsgl_prs','ja_allsgl_prs','ja_rlvsgl_org','ja_allsgl_org','rc_cb_rcdt','rc_ccg','rc_syn','rc_src','am_sug','srh_jobs','sml_jobs','am_inv','google_jobs','fb_jobs','ref_ts','shr_ts','ro_sug','pr_sml_jobs','rc_trrx_inv','syn','src','ref_ptn','ref_cdt','ref_vst_imp','syn_rqt','trr_webinars')
+group by ID, utm_medium
+order by ID desc
