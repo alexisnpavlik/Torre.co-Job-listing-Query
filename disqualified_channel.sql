4bf67213ff66594101b0c0729a5b42dce72e7cc0
laumc98 <lanaca98@gmail.com>
2022-03-17 10:07:48 -0500

    Revert "Update disqualified_channel.sql"
    This reverts commit ed17ee69cd925c8ef070f183a816a83237114434.

--- disqualified_channel.sql
+++ disqualified_channel.sql
@@ -1,15 +1,15 @@
 SELECT
     o.id as ID,
-    oc2.name as Channel,
-    sum(case when oc.id is not null and oc.interested is not null and (last_evaluation.last_not_interest is not null and (last_evaluation.last_interest is null or last_evaluation.last_interest < last_evaluation.last_not_interest)) then 1 else 0 end) as Disqualified,
-    (select Reason from opportunity_changes_history as och where true and type = 'close' and Reason is not null and o.id = och.opportunity_id order by och.created desc limit 1) as Reason
+    `Tracking Codes`.`utm_medium` as utm_medium,
+    sum(case when oc.id is not null and oc.interested is not null and (last_evaluation.last_not_interest is not null and (last_evaluation.last_interest is null or last_evaluation.last_interest < last_evaluation.last_not_interest)) then 1 else 0 end) as Disqualified
 FROM opportunities o
 LEFT JOIN opportunity_candidates oc on o.id=oc.opportunity_id
-LEFT JOIN opportunity_columns oc2 on oc.column_id = oc2.id
-LEFT JOIN (
+LEFT JOIN `tracking_code_candidates` `Tracking Code Candidates - Person` ON `oc`.`id` = `Tracking Code Candidates - Person`.`candidate_id`
+LEFT JOIN `tracking_codes` `Tracking Codes` ON `Tracking Code Candidates - Person`.`tracking_code_id` = `Tracking Codes`.`id`
+left join (
   select me.candidate_id, max(me.interested) as last_interest, max(me.not_interested) as last_not_interest
   from member_evaluations me
   group by me.candidate_id) last_evaluation on last_evaluation.candidate_id = oc.id
-WHERE oc.interested is not null
-group by ID, Channel
+WHERE oc.interested is not null AND utm_medium IN ('ja_mtc','ja_rlvsgl_prs','ja_allsgl_prs','ja_rlvsgl_org','ja_allsgl_org','rc_cb_rcdt','rc_ccg','rc_syn','rc_src','am_sug','srh_jobs','sml_jobs','am_inv','google_jobs','fb_jobs','ref_ts','shr_ts','ro_sug','pr_sml_jobs','rc_trrx_inv','syn','src','ref_ptn','ref_cdt','ref_vst_imp','syn_rqt','trr_webinars')
+group by ID, utm_medium
 order by ID desc
