e385763de4a3729b363c8d296096631dbb1e5d0b
laumc98 <lanaca98@gmail.com>
2022-04-09 12:48:36 -0500

    Update recruiter_chatbot_syn_notifications.sql

--- recruiter_chatbot_syn_notifications.sql
+++ recruiter_chatbot_syn_notifications.sql
@@ -1,4 +1,4 @@
-SELECT TRIM('"' FROM JSON_EXTRACT(no.context, '$.opportunityId')) as AlfaID, count(*) as 'rc_syn_notifications'
+SELECT TRIM('"' FROM JSON_EXTRACT(no.context, '$.opportunityId')) as 'Alfa ID', count(*) as 'rc_syn_notifications'
 FROM notifications no
 WHERE no.template like 'career-advisor-syndication-first-evaluation' and no.status = 'sent'
 GROUP BY 1
