14b258534d3c2dff44ae9f5b577356c5addaafa3
laumc98 <lanaca98@gmail.com>
2022-04-13 11:05:44 -0500

    Create autotrigg_external.sql

---
+++ autotrigg_external.sql
@@ -0,0 +1,11 @@
+select oc.opportunity_id as 'ID', count(*) as 'trigg_ext_notifications'
+from opportunity_candidates oc
+    inner join member_evaluations me on me.candidate_id = oc.id 
+    inner join people p on oc.person_id = p.id
+where (me.interested is not null 
+    and me.not_interested is null
+    and me.send_disqualified_notification = false
+    and oc.column_id is null
+    and oc.application_step is null)
+group by 1
+    
