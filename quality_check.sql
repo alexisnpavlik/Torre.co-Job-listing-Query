c7dc68dd47fb8f428c02cb15f004ba64f60a6795
laumc98 <lanaca98@gmail.com>
2022-04-28 13:58:28 -0500

    Update quality_check.sql

--- quality_check.sql
+++ quality_check.sql
@@ -23,3 +23,4 @@ FROM
 WHERE
     `Opportunity Candidates - Candidate`.`interested` >= date(date_add(now(6), INTERVAL -262 day))
 GROUP BY `member_evaluations`.`candidate_id`,`Opportunity Candidates - Candidate`.`opportunity_id`
+LIMIT BY 500000
