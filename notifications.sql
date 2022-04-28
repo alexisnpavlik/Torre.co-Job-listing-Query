cc999c179fc60ecd6f96dc769176a1412951e5ae
laumc98 <lanaca98@gmail.com>
2022-04-28 17:46:15 -0500

    Update notifications.sql

--- notifications.sql
+++ notifications.sql
@@ -3,5 +3,5 @@ select
     sum(case when model = 'realistic' then 1 else 0 end) as realistic,
     sum(case when model = 'signal_person_all' or model = 'signal_person_relevant' then 1 else 0 end) as people,
     sum(case when model = 'signal_organization_all' or model = 'signal_organization_relevant' then 1 else 0 end) as organization
-from com_torrelabs_match_distributed_2
+from com_torrelabs_match_distributed_3
 group by AlfaID
