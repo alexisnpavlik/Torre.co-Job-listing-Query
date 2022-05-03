faffb4661992ee3434e4b5f49afa8fade9710dfa
laumc98 <lanaca98@gmail.com>
2022-05-03 12:20:48 -0500

    Update recruiter_chatbot_src_notifications.sql

--- recruiter_chatbot_src_notifications.sql
+++ recruiter_chatbot_src_notifications.sql
@@ -1,4 +1,16 @@
-select TRIM('"' FROM JSON_EXTRACT(no.context, '$.opportunityId')) as 'Alfa ID', count(*) as 'rc_src_notifications'
-from notifications no
-WHERE no.template like 'career-advisor-sourcing-first-evaluation' and no.status = 'sent'
+SELECT
+    TRIM('"'FROM JSON_EXTRACT(`notifications`.`context`, '$.opportunityId')) as `AlfaID`,
+    count(*) as `rc_src_notifications`
+FROM
+    `notifications`
+WHERE
+    (
+        (
+            `notifications`.`template` = 'career-advisor-sourcing-first-evaluation-matrix-a'
+            OR `notifications`.`template` = 'career-advisor-sourcing-first-evaluation-matrix-b'
+            OR `notifications`.`template` = 'career-advisor-sourcing-first-evaluation-matrix-c'
+            OR `notifications`.`template` = 'career-advisor-sourcing-first-evaluation'
+        )
+        AND `notifications`.`status` = 'sent'
+    )
 GROUP BY 1
