105ad0fb71877930d45a4a103071dc3b54068421
laumc98 <lanaca98@gmail.com>
2022-05-31 15:01:38 -0500

    Update time_to_3mm.sql

--- time_to_3mm.sql
+++ time_to_3mm.sql
@@ -1,6 +1,6 @@
 SELECT
           opportunity_id AS ID,
-          reviewed AS reviewed_date,
+          date(reviewed) AS reviewed_date,
           date(created) AS match_date,
           datediff(date(created),date(reviewed)) AS time_to_3mm
       FROM
