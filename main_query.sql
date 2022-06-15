cf5f35f3b541bb75b7b79b8c2dcaa745f91b611f
laumc98 <lanaca98@gmail.com>
2022-06-15 17:32:18 -0500

    Update main_query.sql

--- main_query.sql
+++ main_query.sql
@@ -58,8 +58,8 @@ SELECT
     as 'Others',
     -- Hires
     sum(case when oc.id is not null and oc.interested is not null and oc.column_id is not null
-    and oc2.name <> 'hired'
-    and oc2.name <> 'contratados'
+    and oc2.name = 'hired'
+    and oc2.name = 'contratados'
     and (last_evaluation.last_interest is not null and (last_evaluation.last_not_interest is null or last_evaluation.last_interest > last_evaluation.last_not_interest)) then 1 else 0 end)
     as 'Hires pipeline',
     -- Disqualified
