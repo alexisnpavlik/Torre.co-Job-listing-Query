1ad3f22b4f524f88e87db8684f600cf881de99c3
laumc98 <lanaca98@gmail.com>
2022-06-15 13:20:54 -0500

    Update main_query.sql

--- main_query.sql
+++ main_query.sql
@@ -44,9 +44,9 @@ SELECT
     as 'Mutual matches',
     -- Active
     sum(case when oc.id is not null and oc.interested is not null and oc.column_id is not null
-    and SUBSTRING(oc2.name, 1,2) <> '@ '
-    and SUBSTRING(oc2.name, 1,2) <> '#@'
-    and SUBSTRING(oc2.name, 1,2) <> '##'
+    and SUBSTRING(oc2.name, 1,2) <> '@'
+    and SUBSTRING(oc2.name, 1,2) <> '#'
+    and oc2.name <> 'mutual matches'
     and oc2.name <> 'hired'
     and oc2.name <> 'contratados'
     and (last_evaluation.last_interest is not null and (last_evaluation.last_not_interest is null or last_evaluation.last_interest > last_evaluation.last_not_interest)) then 1 else 0 end)
