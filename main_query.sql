f87a14fd9473209a86a6a8e0e0a80f30c4fe4711
laumc98 <lanaca98@gmail.com>
2022-06-15 16:34:11 -0500

    Update main_query.sql

--- main_query.sql
+++ main_query.sql
@@ -56,6 +56,12 @@ SELECT
     and oc2.name <> 'mutual matches'
     and (last_evaluation.last_interest is not null and (last_evaluation.last_not_interest is null or last_evaluation.last_interest > last_evaluation.last_not_interest)) then 1 else 0 end)
     as 'Others',
+    -- Hires
+    sum(case when oc.id is not null and oc.interested is not null and oc.column_id is not null
+    and oc2.name <> 'hired'
+    and oc2.name <> 'contratados'
+    and (last_evaluation.last_interest is not null and (last_evaluation.last_not_interest is null or last_evaluation.last_interest > last_evaluation.last_not_interest)) then 1 else 0 end)
+    as 'Hires',
     -- Disqualified
     sum(case when oc.id is not null and oc.interested is not null
     and (last_evaluation.last_not_interest is not null and (last_evaluation.last_interest is null or last_evaluation.last_interest < last_evaluation.last_not_interest)) then 1 else 0 end)
