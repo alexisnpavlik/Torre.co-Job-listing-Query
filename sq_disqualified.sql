8e0f248802a251c83d488a732933311d7711938a
laumc98 <lanaca98@gmail.com>
2022-07-06 13:53:47 -0500

    Update sq_disqualified.sql

--- sq_disqualified.sql
+++ sq_disqualified.sql
@@ -1,26 +1,36 @@
-/* AA : Sonic : SQ disqualified: prod */ 
+/* AA : Sonic : SQ disqualified: prod */
 SELECT
     `opportunity_candidates`.`id` AS `candidate_id`,
     `opportunity_candidates`.`opportunity_id` AS `Opportunity ID`,
     `Opportunity Questions - Opportunity`.`question_id` AS `question_id`,
+    `opportunity_candidate_responses`.`id` AS `answer_id`,
     `Questions`.`purpose` AS `Questions_purpose`,
     `Questions`.`title` AS `Questions_title`,
     `Questions`.`type` AS `Questions_type`,
     `Questions`.`locale` AS `Questions_locale`
 FROM
     `opportunity_candidates`
-    INNER JOIN `opportunity_questions` `Opportunity Questions - Opportunity` ON `opportunity_candidates`.`opportunity_id` = `Opportunity Questions - Opportunity`.`opportunity_id` AND `Opportunity Questions - Opportunity`.`active` IS TRUE
+    INNER JOIN `opportunity_questions` `Opportunity Questions - Opportunity` ON `opportunity_candidates`.`opportunity_id` = `Opportunity Questions - Opportunity`.`opportunity_id`
+    AND `Opportunity Questions - Opportunity`.`active` IS TRUE
     LEFT JOIN `questions` `Questions` ON `Opportunity Questions - Opportunity`.`question_id` = `Questions`.`id`
     LEFT JOIN `opportunity_candidate_responses` `Opportunity Candidate Responses - Question` ON (
         `opportunity_candidates`.`id` = `Opportunity Candidate Responses - Question`.`candidate_id`
         AND `Opportunity Questions - Opportunity`.`question_id` = `Opportunity Candidate Responses - Question`.`question_id`
-    ) AND `Opportunity Candidate Responses - Question`.`active` IS TRUE
+    )
+    AND `Opportunity Candidate Responses - Question`.`active` IS TRUE
     LEFT JOIN `question_options` `Question Options` ON (
         `Opportunity Candidate Responses - Question`.`question_option_id` = `Question Options`.`id`
         AND `Opportunity Candidate Responses - Question`.`question_id` = `Question Options`.`question_id`
     )
+    LEFT JOIN `opportunity_candidate_responses` ON (
+        `opportunity_candidates`.`id` = `opportunity_candidate_responses`.`candidate_id`
+        AND `Opportunity Questions - Opportunity`.`question_id` = `opportunity_candidate_responses`.`question_id`
+    )
+    AND `opportunity_candidate_responses`.`active` IS TRUE
 WHERE
     (
         `Questions`.`purpose` = 'filter'
+        AND `opportunity_candidate_responses`.`id` IS NOT NULL
     )
-ORDER by `Opportunity ID` asc
+ORDER by
+    `Opportunity ID` asc
