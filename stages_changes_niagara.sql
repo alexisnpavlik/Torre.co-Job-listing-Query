c6dce6f37d372fe50340440f72caa0178b6775e0
laumc98 <lanaca98@gmail.com>
2022-07-20 14:17:42 -0500

    Add stages/channels changes queries

---
+++ stages_changes_niagara.sql
@@ -0,0 +1,9 @@
+/* AA : Sonic : Niagara stages changes : prod */ 
+SELECT
+    state_transition.opportunity_reference_id AS 'Alfa ID',
+    state_transition.current_state,
+    state_transition.active,
+    state_transition.reason_changed,
+    state_transition.timestamp
+FROM
+   state_transition
