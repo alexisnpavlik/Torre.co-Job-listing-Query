071476f04f0430bcb590daab087189da8d08a262
laumc98 <lanaca98@gmail.com>
2022-08-05 14:10:24 -0500

    Add another reason to outbound disqualified

--- disqualified_syn.sql
+++ disqualified_syn.sql
@@ -6,6 +6,7 @@ SELECT
     `Opportunity Candidates`.`interested` AS `Interested_date`,
     max(`member_evaluations`.`not_interested`) AS `Disqualified_date`,
     `member_evaluations`.`reason` AS `Reason disqualified`,
+    `member_evaluations_reason`.`reason` AS `reason_2`,
     `Member Evaluation Feedback - Feedback`.`feedback` AS `Other reason disqualified`,
     `Tracking Codes`.`utm_medium` AS `UTM`
 FROM
@@ -13,7 +14,8 @@ FROM
     LEFT JOIN `opportunity_candidates` `Opportunity Candidates` ON `opportunities`.`id` = `Opportunity Candidates`.`opportunity_id`
     LEFT JOIN `people` ON `Opportunity Candidates`.`person_id` = `people`.`id`
     LEFT JOIN `member_evaluations` ON `Opportunity Candidates`.`id` = `member_evaluations`.`candidate_id` 
-    LEFT JOIN `member_evaluation_feedback` `Member Evaluation Feedback - Feedback` ON `member_evaluations`.`feedback_id` = `Member Evaluation Feedback - Feedback`.`id`
+    LEFT JOIN `member_evaluations_reason` ON `member_evaluations`.`id` = `member_evaluations_reason`.`member_evaluation_id`
+    LEFT JOIN `member_evaluation_feedback` `Member Evaluation Feedback - Feedback` ON `member_evaluations_reason`.`feedback_id` = `Member Evaluation Feedback - Feedback`.`id`
     LEFT JOIN `tracking_code_candidates` `Tracking Code Candidates` ON `Opportunity Candidates`.`id` = `Tracking Code Candidates`.`candidate_id`
     LEFT JOIN `tracking_codes` `Tracking Codes` ON `Tracking Code Candidates`.`tracking_code_id` = `Tracking Codes`.`id` 
     LEFT JOIN `opportunity_members` ON `opportunities`.`id` = `opportunity_members`.`opportunity_id` AND (`opportunity_members`.`manager` = true AND `opportunity_members`.`status` = 'accepted')
