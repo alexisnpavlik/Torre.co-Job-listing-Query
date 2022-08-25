a285ce4b6ad47fdddced5f98e2132551b0f60e49
laumc98 <lanaca98@gmail.com>
2022-08-25 16:13:03 -0500

    Update recruiter_chatbot_ccg_notifications.sql

--- recruiter_chatbot_ccg_notifications.sql
+++ recruiter_chatbot_ccg_notifications.sql
@@ -1,7 +1,16 @@
 /* AA : Sonic : ccg notifications: prod */ 
-SELECT TRIM('"' FROM JSON_EXTRACT(`notifications`.`context`, '$.opportunityId')) as 'Alfa ID', count(*) as 'rc_ccg_notifications'
-FROM `notifications` INNER JOIN `person_flags` `Person Flags - To` ON `notifications`.`to` = `Person Flags - To`.`person_id`
-WHERE (`notifications`.`template` = 'career-advisor-job-opportunity'
-   AND `notifications`.`status` = 'sent'
-   AND `Person Flags - To`.`community_created_claimed_at` is null)
-GROUP BY 1
+SELECT
+    TRIM('"' FROM JSON_EXTRACT(`notifications`.`context`, '$.opportunityId')) as 'Alfa ID',
+    count(*) as 'rc_ccg_notifications'
+FROM
+    `notifications`
+    LEFT JOIN `people` `People - To` ON `notifications`.`to` = `People - To`.`id`
+WHERE
+    (
+        `notifications`.`template` = 'career-advisor-job-opportunity'
+        AND `notifications`.`status` = 'sent'
+        AND `People - To`.`subject_identifier` IS NULL
+        AND `People - To`.`name` not like '%test%'
+    )
+GROUP BY
+    1
